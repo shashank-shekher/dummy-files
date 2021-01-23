edeb790d51919212173b251f0537673128db912f
Shashank Shekher Tripathi <shekharshashank1@gmail.com>
2021-01-23 18:27:54 +0100

    Update dummy.py

--- dummy.py
+++ dummy.py
@@ -1,1 +1,10 @@
-print("Dummy Python file executed")
+import urllib.request
+import hashlib
+
+def hashCode(url):
+    contents = urllib.request.urlopen(url).read()
+    hash256 = hashlib.sha3_256(contents).hexdigest()
+    return hash256
+
+k = hashCode("https://raw.githubusercontent.com/shashank-shekher/dummy-files/main/code.txt")
+print(k)
